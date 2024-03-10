d039200de2d32f21b0fa13794b6d8672e46cd439
Jovuto <153099429+Jovuto@users.noreply.github.com>
2024-03-10 16:09:07 +0000

    feat: add random number 1 to 3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getComputerChoice() {
     // create a random number 1 to 3
+    let choiceNumber = Math.ceil((Math.random()) * 3);
+    console.log(choiceNumber);
     // translate that number into rock, paper, or scissors
     // return the output
 }
