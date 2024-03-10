edc2e6ce356fa033e79891ceeedd6ce166b19048
Jovuto <153099429+Jovuto@users.noreply.github.com>
2024-03-10 16:14:01 +0000

    feat: add choice translation

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function getComputerChoice() {
-    // create a random number 1 to 3
+    // create a random number 1 to 3 and store the variable
     let choiceNumber = Math.ceil((Math.random()) * 3);
     console.log(choiceNumber);
-    // translate that number into rock, paper, or scissors
+    // translate that number into rock, paper, or scissors and store the result as a variable
+    let computerChoice;
+    if (choiceNumber == 1) {
+        computerChoice = "Rock";
+    }
+    else if (choiceNumber == 2) {
+        computerChoice = "Paper";
+    }
+    else if (choiceNumber == 3) {
+        computerChoice = "Scissors";
+    }
+    else {
+        computerChoice = "Something has gone very wrong!";
+    }
+    console.log(computerChoice)
     // return the output
 }
